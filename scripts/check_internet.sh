8f3c61401fd3933e66a3af9950ccab09a6f7a1c8
Michael <michael.ifrah@s.afeka.ac.il>
2026-06-20 00:36:22 +0300

    Added internet check scripts

---
+++ scripts/check_internet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "--- Internet Connection Check Log ---"
+echo "Date: $(date)"
+
+# Ping Google DNS once to check connection
+ping -c 1 8.8.8.8 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "Status: Connected to the internet."
+else
+    echo "Status: No internet connection."
+fi
+echo "-------------------------------------
+
+
+
