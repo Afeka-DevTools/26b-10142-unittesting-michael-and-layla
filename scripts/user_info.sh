38b517cf28a3939eeb1ac79f35f374002770fd1b
Michael <michael.ifrah@s.afeka.ac.il>
2026-06-20 00:47:43 +0300

    Added user info scripts

---
+++ scripts/user_info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "--- User Information ---"
+echo "Username: $USER"
+echo "Home Directory: $HOME"
+echo "Groups: $(groups)"
+echo "Current Shell: $SHELL"
+echo "------------------------"
